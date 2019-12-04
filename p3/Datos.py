4930e80c1ad9380deebb852060175e2ee2bc3d81
Fons <fons@Fonss-MacBook-Air.local>
2019-12-04 03:51:13 +0100

    principio clase acabada pero comprobar si falla

---
+++ p3/Datos.py
@@ -0,0 +1,121 @@
+import numpy as np
+
+class Datos:
+
+  TiposDeAtributos=('Continuo','Nominal')
+
+  def __init__(self, nombreFichero, oneHot = False):
+    f = open( nombreFichero, "r")
+    nDatos = int(f.readline()) # Unused?
+    #Read attributes names line and store it
+    self.nombreAtributos = f.readline()[:-1].split(',')
+
+    #Read attributes types line and store it. Check for invalid types.
+    self.tipoAtributos = f.readline()[:-1].split(',')
+
+    # Check that the attributes in the dataset match the attributes we expect
+    for tipo in self.tipoAtributos:
+        if tipo not in self.TiposDeAtributos:
+            raise ValueError(tipo)
+
+    # List containing True in the ith position if the ith atribute is nominal, False otherwise
+    self.nominalAtributos = [True if i == 'Nominal' else False for i in self.tipoAtributos ]
+
+    # We create a list whose elements are lists containing the data, and we get a no.array from it
+    datos = []
+    for line in f:
+        datos.append(line[:-1].split(','))
+    self.datos = np.array(datos)
+
+    # For each column (attribute) of the matrix we need to create a dictionary
+    self.diccionarios = []
+    i = 0;
+    for att in self.nominalAtributos:
+        temp_dic = {}
+        if att == True:
+            #if the attribute is Nominal create a list with the ith column
+            #of the array "datos"
+            column = self.datos[:, i]
+            #Now we create a set out of each column
+            #This automatically orders the elements alphabetically
+            column_set = sorted(set(column))
+
+            #Now we see which elements belong to the set
+            #and we add it to the dictionary
+            temp_dic = {}
+            value = 0
+            for item in column_set:
+                temp_dic[item] = value
+                value = value + 1
+        #if the attribute is Continuous append an empty dictionary
+        self.diccionarios.append(temp_dic)
+        i = i + 1
+
+    #Now we have the list of dictionaries we have to modify the
+    #matrix Datos and change it's values to the corresponding numbers in
+    #our dictionaries for each column
+    #For each column
+    nAtributos = len(self.nominalAtributos)
+    for col in range(0, nAtributos):
+        #if the attribute is nominal
+        if self.nominalAtributos[col] == True:
+            #We replace each value with the corresponding value of the
+            #corresponding dictionary
+            for row in range(0, nDatos):
+                self.datos[row, col] = self.diccionarios[col][self.datos[row, col]]
+
+    #We change the matrix datatype to Float
+    self.datos = self.datos.astype(dtype=float)
+    
+    if oneHot == True:
+        # Create new matrix with desired dimensions
+        # Binary attributes (len = 2) must count as an extra column and not as 2 new columns.
+        newNCols = sum([1 if len(diccionario) == 2 else max(1, len(diccionario)) for diccionario in self.diccionarios])
+        newDatos = np.empty([len(datos), newNCols])
+
+        # initialize a new atributos array
+        # Maybe in the future it is also necessary to update other self.thingies?
+        # self.dictionaries would be an array of empty dicts, idk. It might be necessary at some point
+        nombreAtributos = []
+        # Lets update nominalAtributos as well.
+        nominalAtributos = []
+
+        newi = 0
+        for oldi in range(len(self.nominalAtributos)):
+            if self.nominalAtributos[oldi] == False:
+                # update matrix
+                newDatos[:, newi] = self.datos[:, oldi]
+                # update nombreAtributos, nominalAtributos
+                nombreAtributos.append(self.nombreAtributos[oldi])
+                nominalAtributos.append(self.nominalAtributos[oldi])
+                newi += 1
+            else:
+                keys =  self.diccionarios[oldi].keys()
+                if len(keys) == 2: # Only 2 arguments: binary attribute -> no extra column
+                    # update matrix
+                    value = self.diccionarios[oldi][list(keys)[0]]
+                    newcol = [1 if self.datos[k, oldi] == value else 0 for k in range(len(self.datos))]
+                    newDatos[:, newi] = newcol 
+                    # update nombreAtributos, nominalAtributos
+                    nombreAtributos.append(self.nombreAtributos[oldi] + '_' + list(keys)[0]) 
+                    nominalAtributos.append(True)
+                    newi += 1
+                else:
+                    for key in keys:
+                        # update matrix
+                        value = self.diccionarios[oldi][key]
+                        newcol = [1 if self.datos[k, oldi] == value else 0 for k in range(len(self.datos))]
+                        newDatos[:, newi] = newcol 
+                        # update nombreAtributos, nominalAtributos
+                        nombreAtributos.append(self.nombreAtributos[oldi] + '_' + key) 
+                        nominalAtributos.append(True)
+                        newi += 1
+        self.datos = newDatos
+        self.nombreAtributos = nombreAtributos
+        self.nominalAtributos = nominalAtributos
+    f.close()
+
+  #Recibe en idx una lista de indices que extraer y devolver de la
+  #Matriz datos
+  def extraeDatos(self, idx):
+      return self.datos[idx]
---
+++ p3/tries.py
@@ -0,0 +1,170 @@
+import Cromosoma as cr
+from Datos import Datos
+import numpy as np
+import Clasificador as cl
+import EstrategiaParticionado as ep
+
+################################################################################################
+# Prueba de la clase Cromosoma
+################################################################################################
+# numReglas = 1
+# len atrib1 = 3 (puede tomar vals 0,1,2)
+# len atrib2 = 2 (puede tomar vals 0,1)
+# len atrib3 = 2 (puede tomar vals 0,1)
+# c1 = [0,0,0,0,0,0,0,0]
+# c2 = [1,1,1,1,1,1,1,1]
+
+# numReglas = 1
+# seed = 1
+# lensAtributos = [3,2,2]
+# lenRegla = sum(lensAtributos) + 1
+# p = 2
+# r1 = np.array([0 for i in range(0,lenRegla * numReglas)])
+# r2 = np.array([1 for i in range(0,lenRegla * numReglas)])
+# c1 = cr.Cromosoma(numReglas, lensAtributos, reglas=r1)
+# c2 = cr.Cromosoma(numReglas, lensAtributos, reglas=r2)
+#
+# # c1 = [0,0,0,0,0,0,0,0]
+# # c2 = [1,1,1,1,1,1,1,1]
+# print("Inicio")
+# print("C1 == ", c1.reglas)
+# print("C2 == ", c2.reglas)
+#
+# # c1 = [1,1,0,0,0,0,0,0]
+# # c2 = [0,0,1,1,1,1,1,1]
+# c1.cruzar(c2, p)
+# print("Despues Cruce")
+# print("C1 == ", c1.reglas)
+# print("C2 == ", c2.reglas)
+#
+# c1.mutar()
+# print("Despues Mutar C1")
+# print("C1 == ", c1.reglas)
+# print("C2 == ", c2.reglas)
+#
+# c2.mutar()
+# print("Despues Mutar C2")
+# print("C1 == ", c1.reglas)
+# print("C2 == ", c2.reglas)
+#
+# c3 = cr.Cromosoma(numReglas*2, lensAtributos)
+# print("Despues Crear C3")
+# print("C1 == ", c1.reglas)
+# print("C3 == ", c3.reglas)
+# c1.cruzar(c3, 2)
+#
+# print("Despues Cruzar C3 C1")
+# print("C1 == ", c1.reglas)
+# print("C3 == ", c3.reglas)
+#
+#
+# dato = np.array([2,0,1])
+# print("dato == ", dato)
+# encoded = c1.encode(dato)
+# # encoded = [0 0 1 1 0 0 1]
+# print("encoded == ", encoded)
+# datos = np.array([[0]*(lenRegla), [1]*(lenRegla)])
+# # datos = [[0 0 0 0 0 0 0 0] [1 1 1 1 1 1 1 1]]
+# print("datos == ", datos)
+# # c1 = [1,1,0,0,0,0,0,0] con 1 bit mutado
+# print("reglas C1 == ", c1.reglas)
+# # deberia predecir ambos a 0 (porq c1 no 'reconoce' a ningun dato)
+# print("\nBEGIN FITNESS CALCULATIONS\n")
+# fc11 = c1.calcularFitness(datos=datos)
+# print("Fitness c1 == ", fc11)
+# r4 = np.append(c1.reglas, datos[0])
+# r4 = np.append(r4, datos[1])
+# c4 = cr.Cromosoma(3, lensAtributos, reglas=r4)
+# print("reglas C4 == ", c4.reglas)
+# fc41 = c4.calcularFitness(datos=datos)
+#
+# # Datos mas reales
+# newdatos = np.array([[2, 0, 1], [2, 1, 0], [1, 1, 1]])
+# print("new datos ==\n ", newdatos)
+# encDatos = np.array([np.append(c4.encode(row), 0) for row in newdatos[:,:-1]])
+# clases = newdatos[:,-1]
+# encDatos[:,-1] = clases
+# lensAtributos = [3,2]
+# print("Encoded matrix ==\n ", encDatos)
+# r5 = np.append(encDatos[0], encDatos[1])
+# c5 = cr.Cromosoma(2, lensAtributos, reglas=r5)
+# fc51 = c5.calcularFitness(datos=encDatos)
+# print("Fitness c5 == ", fc51)
+
+
+# Ejemplo para que se vea del todo lo que esta pasando
+# 3 atributos
+#   atrib1 toma valores 0,1,2
+#   atrib2 toma valores 0,1
+#   atrib3 toma valores 0,1
+#
+# datos entrenamiento (matriz)
+#   datos = [[0, 0, 0, 1], [1, 1, 1, 1]]
+#       codificados = [[10010101][01001011]]
+# 3 cromosomas
+#  cBueno = [11111111] siempre predice 1 (por suerte)
+#  cMedio = [11111111 10010100] predice 1 (y 0 para dato 2)
+#  cMalo = [11111110] predice siempre 0
+# lensAtributos = [3, 2, 2]
+# rBueno = np.array([1]*(sum(lensAtributos) + 1))
+# rMedio = np.append(np.array([1]*(sum(lensAtributos) + 1)), np.array([1,0,0,1,0,1,0,0]))
+# rMalo = np.array([1]*(sum(lensAtributos) + 1))
+# rMalo[sum(lensAtributos)] = 0
+# datos = np.array([[0, 0, 0, 1], [1, 1, 1, 1]])
+# print("Datos:\n ", datos)
+#
+#
+# cBueno = cr.Cromosoma(1, lensAtributos, reglas=rBueno)
+# cMedio = cr.Cromosoma(2, lensAtributos, reglas=rMedio)
+# cMalo = cr.Cromosoma(1, lensAtributos, reglas=rMalo)
+# encDatos = np.array([np.append(cBueno.encode(row), 0) for row in datos[:,:-1]])
+# clases = datos[:,-1]
+# encDatos[:,-1] = clases
+# print("Datos Codificados:\n ", encDatos)
+# print("cBueno: ", cBueno.reglas)
+# print("cMedio: ", cMedio.reglas)
+# print("cMalo: ", cMalo.reglas)
+# fBueno = cBueno.calcularFitness(datos=encDatos)
+# fMedio = cMedio.calcularFitness(datos=encDatos)
+# fMalo = cMalo.calcularFitness(datos=encDatos)
+# print("Fitness cBueno: ", fBueno)
+# print("Fitness cMedio: ", fMedio)
+# print("Fitness cMalo: ", fMalo)
+#
+#
+# # prueba crear cromosoma aleatorio:
+# cAl = cr.Cromosoma(1, lensAtributos)
+# print("reglas al == ", cAl.reglas)
+
+
+
+################################################################################################
+# dataset=Datos('DatasetEjemplo/ejemplo1.data')
+# dataset=Datos('DatasetEjemplo/tic-tac-toe.data')
+# diccionario = dataset.diccionarios
+# print(diccionario)
+# nReglas = 3
+# nAtributos = len(diccionario) - 1
+# lensAtributos = [len(diccionario[i]) for i in range(0, nAtributos)]
+# lenRegla = sum(lensAtributos) + 1
+#
+# print("nAtributos = 9 --> ", nAtributos)
+# print("lenAtributos = [3, 3, 3...3] --> ", lensAtributos)
+# print("lenRegla = 3 + 3 ... + 3== 28 --> ", lenRegla)
+
+# dataset=Datos('DatasetEjemplo/tic-tac-toe.data')
+dataset=Datos('DatasetEjemplo/ejemplo1.data')
+dicc = dataset.diccionarios
+
+# datosTrain = dataset.datos[:50]
+# print(datosTrain)
+gen = cl.ClasificadorGenetico(tamPoblacion=100, nEpocas=100, seed=3)
+errors = []
+for i in range(5):
+    es = ep.ValidacionSimple(20)
+    errors.append(gen.validacion(es,dataset,gen))
+errorsnp = np.array(errors)
+mean,std = np.mean(errors), np.std(errors)
+print("MEAN ERRORS == ", mean, " STD == ", std)
+# # gen.entrenamiento(datosTrain, dicc, dicc)
+# datosTest = dataset.datos[50:]
